eb489a7e9cbb74b311b24f68a9571faec071f1e6
gafoit <74756986+gafoit@users.noreply.github.com>
2025-07-17 23:55:59 +0700

    Aboba

--- Bot/handlers/modes.py
+++ Bot/handlers/modes.py
@@ -7,6 +7,6 @@ router = Router()
 text = 'Раздел модулей приложения. Выберите интересующий чтобы узнать больше.'
 
 
-@router.message(F.text == "🪜 Модули проекта")
+@router.message(F.text.contains("🪜 Модули проекта"))
 async def mode_handler(message: types.Message):
     await message.answer(text, reply_markup=modes_keyboard())
