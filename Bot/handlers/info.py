734dab7dbd6f5032fa687de8d9d0332d16393f5b
gafoit <74756986+gafoit@users.noreply.github.com>
2025-07-17 20:31:01 +0700

    Aboba

--- Bot/handlers/info.py
+++ Bot/handlers/info.py
@@ -1,11 +1,11 @@
 from aiogram import Router, types
 from aiogram import F
+
 router = Router()
+
 
 @router.message(F.text == "ℹ️ О проекте")
 async def info_command(message: types.Message):
     await message.answer("Это Telegram-бот для описания проекта. Используйте меню ниже.")
 
-@router.message()
-async def catch_all(message: types.Message):
-    print("==> Message received:", message)
+
--- Bot/handlers/start.py
+++ Bot/handlers/start.py
@@ -4,6 +4,6 @@ from aiogram import F
 router = Router()
 
 
-@router.message(F.command("start"))
+@router.message(F.text == "/start")
 async def cmd_start(message: types.Message):
     await message.answer("Добро пожаловать! Выберите режим:", reply_markup=main_menu_keyboard())
