9604d127be275ed42424c2eeeb85514d6c7634b8
gafoit <74756986+gafoit@users.noreply.github.com>
2025-07-18 02:08:36 +0700

    Aboba

--- Bot/keyborads/modes.py
+++ Bot/keyborads/modes.py
@@ -8,8 +8,7 @@ def modes_keyboard():
     buttons = [
         [KeyboardButton(text="🟠 Ментальная Арифметика"), KeyboardButton(text="🟠 Общий счёт")],
         [KeyboardButton(text="🟣 Концентрация")],
-        [KeyboardButton(text="🟠Найди пару")],
-        [KeyboardButton(text="🟣 Шульте")],
+        [KeyboardButton(text="🟠 Найди пару"), KeyboardButton(text="🟠 Шульте")],
         [KeyboardButton(text="🔙 В Меню")],
     ]
     return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
