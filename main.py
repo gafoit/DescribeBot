066b6f3ffcf86c9cd70c5093219873366f492594
gafoit <74756986+gafoit@users.noreply.github.com>
2025-07-17 19:20:45 +0700

    Aboba

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ from Bot.bot import bot, dp
 from Bot import config
 from Bot.handlers import start, info
 
-# Логирование
 logging.basicConfig(level=logging.INFO)
 
-# Роутеры
 dp.include_router(start.router)
 dp.include_router(info.router)
 
 
 async def on_startup():
+    logging.info(f"Setting webhook to {config.WEBHOOK_URL}")
     await bot.set_webhook(config.WEBHOOK_URL, secret_token=config.WEBHOOK_SECRET)
 
 
 async def on_shutdown():
+    logging.info("Deleting webhook")
     await bot.delete_webhook()
 
 
@@ -26,8 +26,11 @@ async def create_app():
     app = web.Application()
     app["bot"] = bot
 
-    # Для обработки входящих запросов
-    webhook_requests_handler = SimpleRequestHandler(dispatcher=dp, bot=bot, secret_token=config.WEBHOOK_SECRET)
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=config.WEBHOOK_SECRET
+    )
     webhook_requests_handler.register(app, path=config.WEBHOOK_PATH)
 
     setup_application(app, dp, on_startup=on_startup, on_shutdown=on_shutdown)
@@ -35,7 +38,5 @@ async def create_app():
 
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    app = loop.run_until_complete(create_app())
-    web.run_app(app, port=8000)
+    # web.run_app(create_app(), port=8000)
+    asyncio.run(create_app()).run_app(port=8000)
