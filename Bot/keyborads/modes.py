a68a15d712b20d750aa3ce3df23603a7783f40e1
gafoit <74756986+gafoit@users.noreply.github.com>
2025-07-18 20:02:43 +0700

    Обновил кнопку МА

--- Bot/keyborads/modes.py
+++ Bot/keyborads/modes.py
@@ -6,9 +6,10 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def modes_keyboard():
     buttons = [
-        [KeyboardButton(text="🟠 Ментальная Арифметика"), KeyboardButton(text="🟠 Общий счёт")],
+        [KeyboardButton(text='<a href="https://telegra.ph/Mentalnaya-arifmetika-07-18">🟠 Ментальная Арифметика</a>'),
+         KeyboardButton(text="🟠 Общий счёт")],
         [KeyboardButton(text="🟣 Концентрация")],
         [KeyboardButton(text="🟠 Найди пару"), KeyboardButton(text="🟠 Шульте")],
         [KeyboardButton(text="🔙 В Меню")],
     ]
-    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True, input_field_placeholder="Воспользуйтесь меню:")
