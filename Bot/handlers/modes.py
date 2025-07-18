8fb9a71b1517261e4e0edc36f02cf29e0ce372f9
gafoit <74756986+gafoit@users.noreply.github.com>
2025-07-18 19:42:45 +0700

    Поменял клавиатуру для неопознанных сообщений

--- Bot/handlers/modes.py
+++ Bot/handlers/modes.py
@@ -7,6 +7,11 @@ router = Router()
 text = 'Раздел модулей приложения. Выберите интересующий чтобы узнать больше.'
 
 
-@router.message(F.text=="🔡 Модули проекта")
+@router.message(F.text == "🔡 Модули проекта")
 async def mode_handler(message: types.Message):
     await message.answer(text, reply_markup=modes_keyboard())
+
+
+@router.message(F.text == "🟠 Ментальная Арифметика")
+async def mental_math(msg: types.Message):
+    await msg.answer('<a href="https://telegra.ph/Mentalnaya-arifmetika-07-18">', parse_mode='HTML')
