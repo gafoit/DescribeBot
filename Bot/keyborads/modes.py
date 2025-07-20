e2c6ce0fe767d6aa0085f4a5497700f7d7145afd
gafoit <74756986+gafoit@users.noreply.github.com>
2025-07-20 17:14:57 +0700

    Ещё что-то

--- Bot/keyborads/modes.py
+++ Bot/keyborads/modes.py
@@ -19,9 +19,9 @@ def inline_modes_keyboard():
     buttons = [
         [InlineKeyboardButton(text='🟠 Ментальная Арифметика',
                               url="https://telegra.ph/Rezhim-Formuly-dlya-mentalnoj-arifmetiki-07-20"),
-         InlineKeyboardButton(text='🟠 Общий счёт', url="https://telegra.ph/Mentalnaya-arifmetika-07-18")],
-        [InlineKeyboardButton(text="🟣 Концентрация", url="https://telegra.ph/Mentalnaya-arifmetika-07-18")],
-        [InlineKeyboardButton(text="🟠 Найди пару", url="https://telegra.ph/Mentalnaya-arifmetika-07-18"),
-         InlineKeyboardButton(text="🟠 Шульте", url="https://telegra.ph/Mentalnaya-arifmetika-07-18")],
+         InlineKeyboardButton(text='🟠 Общий счёт', url="https://telegra.ph/Modul-Obshchij-schyot-07-20")],
+        [InlineKeyboardButton(text="🟣 Концентрация", url="https://telegra.ph/Modul-Koncentraciya-07-20")],
+        [InlineKeyboardButton(text="🟠 Найди пару", url="https://telegra.ph/Modul-Najdi-Paru-07-20"),
+         InlineKeyboardButton(text="🟠 Шульте", url="https://telegra.ph/Modul-Tablica-SHulte-07-20")],
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
