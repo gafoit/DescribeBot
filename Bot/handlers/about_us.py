638c065ba15743a71f0a201be1f7290bae7f68bc
gafoit <74756986+gafoit@users.noreply.github.com>
2025-07-21 21:39:02 +0700

    Обновление "О нас"

--- Bot/handlers/about_us.py
+++ Bot/handlers/about_us.py
@@ -7,17 +7,23 @@ from Bot.keyborads.back import back_keyboard
 
 router = Router()
 
-about_us_text = """
-О нас.
-
-"""
+about_us_text = "О нас.\n\n" \
+                "<b>Глава проекта</b>\n" \
+                "<i>Долбня Анна Панагиотисовна</i>\n" \
+                'Студентка 4 курса КемГУ по направлению "Прикладная математика и информатика".\n' \
+                "Идейный вдохновитель проекта.\n\n" \
+                "<b>Программист проекта</b>" \
+                "<i>Ступников Даниил Игоревич</i>\n" \
+                'Студент 4 курса КемГУ по направлению "Прикладная математика и информатика".\n' \
+                "Главный программист проекта во всех направлениях, от Desktop-приложений до Telegram-ботов и Web-программирования\n\n" \
+                "Ниже можно найти наши контакты для связи."
 
 
 @router.message(F.text == "©️ О нас")
 async def about_us(message: types.Message):
-    await message.answer_media_group(media_group.MediaGroupBuilder(
-        [types.InputMediaPhoto(media=types.FSInputFile(os.getcwd() + "/Bot/assets/MyLoveAnny.jpg")),
-         types.InputMediaPhoto(media=types.FSInputFile(os.getcwd() + "/Bot/assets/MyMe.jpg"))],
-        caption=about_us_text
-    ).build())
-    # await message.answer(about_us_text, reply_markup=back_keyboard())
+    await message.answer_media_group(
+        media_group.MediaGroupBuilder(
+            [types.InputMediaPhoto(media=types.FSInputFile(os.getcwd() + "/Bot/assets/MyLoveAnny.jpg")),
+             types.InputMediaPhoto(media=types.FSInputFile(os.getcwd() + "/Bot/assets/MyMe.jpg"))],
+            caption=about_us_text).build(),
+        reply_markup=back_keyboard())
